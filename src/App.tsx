f6e24c8016c96afeff2c91679e129ffe8a94d6d8
Sowmya <sowmya.dhandapani@flexibees.com>
2024-12-12 18:51:30 +0530

    Reordering the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Authenticator, Card, Collection, Flex, Heading, Label, Link, withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Card, Collection, Flex, Heading, Label, Link, Tabs, withAuthenticator } from '@aws-amplify/ui-react';
 import { useEffect, useState } from 'react';
 import { generateClient } from "aws-amplify/data";
 import { FormSchema } from '../amplify/data/resource';
@@ -17,53 +17,49 @@ function App() {
   useEffect(() => {
     fetchQuestions();
   }, []);
+  function getHistory() {
+    return <>
+    <Flex id='Read' >
+      <Collection items={questions} type="list" isPaginated={true} isSearchable={true} itemsPerPage={2} column={3} row={5}>
+        {(item, index) =>
+          <Card key={index} backgroundColor={"inherit"} border={"brown"}>
+            <Label children={item.interviewer} /> evaluated <Label children={item.candidateName} /> on <Label children={item.createdAt} />, <p></p>
+            Learning Readiness: <Label children={item.learningReadiness} /> , <p></p>
+            Marital Status: <Label children={item.maritalStatus} /><p></p>
+            Care Giving:  <Label children={item.careGiving} /><p></p>
+            Comment: <Label children={item.comment} /><p />
+          </Card>
+        }
+      </Collection>
+    </Flex>
+    </>
+  }
+  function getForm() {
+    return <>
+     <Heading level={5}> Fill the form based on your feedback from the interview. </Heading>
+     <Flex id="Create"  >
+        <QuestionCreateForm />
+    </Flex>
+    </>
+  }
   return (
     <Flex padding={"medium"} >
-    <Authenticator>
-      {({ signOut }) => (
-        <main>
-          <Heading level={1} children="Flexifit Feedback" alignSelf={"center"}></Heading>        
+      <Authenticator>
+        {({ signOut }) => (
+          <main>
+            <Heading level={1} children="Flexifit Feedback" alignSelf={"center"}></Heading>
 
-          <Link children="Signout" onClick={signOut} alignSelf={"end"}/>
-          
-          <Flex justifyContent="space-between" paddingTop={"large"}>
-              <Collection items={questions} type="grid" isPaginated={true} isSearchable={true} itemsPerPage={15} column={3} row={5}>
-                {(item, index) =>
-                  <Card key={index} backgroundColor={"inherit"} > 
-                    <Label children={item.interviewer} /><p></p>
-                    <Label children={item.candidateId} /><p></p>
-                    <Label children={item.comment} /><p />
-                    </Card>
-              }
-                  </Collection>
-{/* 
-          <Table title = "Existing List" variation='bordered' >
-            <TableHead>
-                <TableCell children= "Interviewer" />
-                <TableCell children= "candidateId"/>
-               <TableCell children="comment" />
-            </TableHead>
-            {questions.map(question =>
-              <TableRow>
-                <TableCell children={question.interviewer} />
-                <TableCell children={question.candidateId}/>
-               <TableCell children={question.comment} />
-              </TableRow>
-            )}
-          </Table> */}
-          </Flex>
-          <p>
-
-
-          </p>
-          <Flex justifyContent="space-between" >
-            <Heading level={5}> New Feedback: Fill the form based on your feedback from the interview. All fields are required </Heading>
-          </Flex>
-          <QuestionCreateForm />
-        </main>
-      )}
-    </Authenticator>
-      </Flex>
+            <Link children="Signout" onClick={signOut} alignSelf={"end"} />
+              <Tabs defaultValue="create" justifyContent={"stretch"}
+                items={[
+                  { label: 'Create', value: 'create', content:  (getForm())  },
+                  { label: 'History', value: 'read', content: (getHistory())  },
+                ]}
+              />
+          </main>
+        )}
+      </Authenticator>
+    </Flex>
   );
 }
 export default withAuthenticator(App);
